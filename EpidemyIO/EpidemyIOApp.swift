7b58875374084e8cf3ce39156311699182d54efd
Oronemu <Oronemu@mail.ru>
2023-05-06 14:56:35 +0700

    Initial commit

---
+++ EpidemyIO/EpidemyIOApp.swift
@@ -0,0 +1,17 @@
+//
+//  EpidemyIOApp.swift
+//  EpidemyIO
+//
+//  Created by Иван Ровков on 06.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct EpidemyIOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
