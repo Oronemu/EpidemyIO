1c862ce768481e3755b960f35b08621e7f22284f
Oronemu <Oronemu@mail.ru>
2023-05-11 12:03:30 +0700

    Added comments

--- EpidemyIO/Model/SimulationModel.swift
+++ EpidemyIO/Model/SimulationModel.swift
@@ -7,21 +7,25 @@
 
 import Foundation
 
+// MARK: - Параметры необходимые для симуляции заражения
 class SimulationModel: ObservableObject {
 	
-	private var infectorIndicies: [Int] = []
-	private var group: [Person] = []
-	private var timer: DispatchSourceTimer?
+	// MARK: - Параметры необходимые для симуляции заражения
+	
+	private var infectorIndicies: [Int] = [] // Массив индексов заразителей, т.е. только тех кто может заразить
+	private var group: [Person] = [] // Массив со всей группой
+	private var timer: DispatchSourceTimer? // Таймер для работы в фоне
 
-	private var infectionInterval: Int
-	private var InfectionFactor: Int
-	private var columns: Int
-	private var groupSize: Int
+	private var infectionInterval: Int // Интервал с которым будет происходить заражение
+	private var InfectionFactor: Int // Количество людей, которых может заразить зараженный
+	private var columns: Int // Количество колонок из View. Нужно для расчета соседей
+	private var groupSize: Int // Размер группы людей
 	
-	private var healthyPeople: Int = 0
-	private var infectedPeople: Int = 0
+	// MARK: - Счетчики здоровых/больных людей
 	
-	private var callback: (Int, Int) -> Void
+	private var healthyPeople: Int = 0 // Счетчик здоровых людей
+	private var infectedPeople: Int = 0 // Счетчик больных людей
+	private var callback: (Int, Int) -> Void // Callback для передачи значений счетчиков модели ViewModel'е
 	
 	init(groupSize: Int, infectionInterval: Int, InfectionFactor: Int, columns: Int, callback: @escaping (Int, Int) -> Void) {
 		self.groupSize = groupSize
@@ -31,52 +35,65 @@ class SimulationModel: ObservableObject {
 		self.callback = callback
 	}
 	
+	// MARK: - Метод создания группы
+	//
+	// Выполняем создание группы в отдельном потоке, чтобы приложение
+	// не зависало при генерации слишком большой группы.
 	func createGroup(_ completion: @escaping ([Person]) -> Void ){
 		DispatchQueue.global(qos: .background).async {
 			let group = (0...self.groupSize-1).map { _ in Person() }
 			self.healthyPeople = group.count
 			DispatchQueue.main.async {
 				self.group = group
-				completion(group)
+				completion(group) //Как только группа будет готова, то отслыаем ее в главный поток для отображения
 			}
 		}
 	}
 	
+	// MARK: - Метод запуска симуляции
+	//
+	// Данный метод запускает нашу симуляцию в background потоке и бесконечно выполняет алгоритм распространения инфекции
 	func startSimulation() {
-		if timer == nil {
-			timer = DispatchSource.makeTimerSource(queue: DispatchQueue.global(qos: .background))
+		if timer == nil { // Чтобы по случайности не запустить несколько симуляций проверяем не запускали ли мы ее ранее
+			timer = DispatchSource.makeTimerSource(queue: DispatchQueue.global(qos: .background)) // Назначаем timer в background очередь
 			timer?.schedule(deadline: .now(), repeating: Double(infectionInterval))
-			timer?.setEventHandler(handler: simulationLoop)
+			timer?.setEventHandler(handler: simulationLoop) // Для удобства выносим нашу симуляцию в отдельную функцию
 			timer?.resume()
 		}
 	}
 	
+	// MARK: - Метод остановки симуляции
 	func stopSimulation() {
 		timer?.cancel()
-		timer = nil
+		timer = nil // Отменяем запланированную задачу и зануляем таймер
 		self.group.removeAll()
-		self.infectorIndicies.removeAll()
+		self.infectorIndicies.removeAll() // Очищаем групу со всеми людьми и массив индексов заразителей
 	}
 	
+	// MARK: - Метод заражения человека
+	//
+	// Получаем индекс выбранного из массива человека и добавляем его в массив заражателей
 	func addToInfectors(infectorIndex: Int, _ completion: @escaping (Int, Int) -> Void) {
 		self.group[infectorIndex].infect()
 		self.infectorIndicies.append(infectorIndex)
-		self.incrementInfectedPeopleCount()
-		completion(self.infectedPeople, self.healthyPeople)
+		self.incrementInfectedPeopleCount() // Увеличиваем счетчик зараженных и здоровых
+		completion(self.infectedPeople, self.healthyPeople) // Возвращаем эти счетчики для ViewModel'и
 	}
-	 
+	
+	// MARK: - Метод инкрементирования счетчиков
 	private func incrementInfectedPeopleCount() {
 		self.infectedPeople += 1
 		self.healthyPeople -= 1
 	}
 	
+	// MARK: - Симуляция распространения заражения
 	private func simulationLoop() {
-		let currentInfectorIndicies = self.infectorIndicies
-		self.infectorIndicies = []
+		let currentInfectorIndicies = self.infectorIndicies // Копируем текущую волну заражателей в локальный массив
+		self.infectorIndicies = [] // Зануляем заражателей, чтобы подготовить массив к следующий волне заражателей
 		
-		for infectorIndex in currentInfectorIndicies {
-			let neighbours = getHealthyNeighbours(of: infectorIndex).shuffled()
-			guard !neighbours.isEmpty else { continue }
+		for infectorIndex in currentInfectorIndicies { //Итерируемся по каждому заражателю
+			let neighbours = getHealthyNeighbours(of: infectorIndex).shuffled() //Для каждого из них определяем здоровых соседей
+			guard !neighbours.isEmpty else { continue } //Если у текущего заражетял нет соседей, идем к следующему
 			
 			var range: Int {
 				if neighbours.count > InfectionFactor {
@@ -86,40 +103,43 @@ class SimulationModel: ObservableObject {
 				}
 			}
 			
-			let neighbourInfectors = neighbours[0..<range]
+			let neighbourInfectors = neighbours[0..<range] // Берем только нужное количество соседей меньшее чем InfectionFactor
 						
-			for neighbour in neighbourInfectors {
-				neighbour.person.infect()
+			for neighbour in neighbourInfectors { // Итерирусемся по каждому соседу
+				neighbour.person.infect() // Заражаем его
 				self.incrementInfectedPeopleCount()
-				self.infectorIndicies.append(neighbour.index)
+				self.infectorIndicies.append(neighbour.index) // Инкрементируем счетчик и добавляем этого сосдеа в следующую волну заражателей
 			}
 		}
-		DispatchQueue.main.async {
+		DispatchQueue.main.async { // Как только просчитаем всех зараженных за волну, обновляем счетчик во ViewModel
 			self.callback(self.infectedPeople, self.healthyPeople)
 		}
 	}
 	
+	// MARK: - Получение соседей элемента массива
 	private func getHealthyNeighbours(of index: Int) -> [(person: Person, index: Int)]{
-		let n = 1
+		let n = 1 // Задаем радиус в котором нужно искать соседей, больший радиус включает и меньшие радиусы
+							// Иными словами, если будет n = 2, то мы будем искать среди не 8 соседей, а 24
 		var res: [(Person, Int)] = []
 		
+		// Определяем соедей по всем сторонам
 		let top = max(0, index / columns - n)
 		let bottom = min((group.count - 1) / columns, index / columns + n)
 		let left = max(0, index % columns - n)
 		let right = min(columns - 1, index % columns + n)
 		
 		for row in top...bottom {
-			for column in left...right {
+			for column in left...right { // Перебираем всех соседей вокруг нашего элемента
 				let currentIdx = row * columns + column
-				if index != currentIdx && currentIdx < group.count {
-					let currentPerson = group[currentIdx]
-					if !currentPerson.isInfected {
+				if index != currentIdx && currentIdx < group.count { //Если сосед существует, то
+					let currentPerson = group[currentIdx] //получаем индекс соседа из группы людей
+					if !currentPerson.isInfected { // Если этот сосед не заражен, то добавляем его в массив
 						res.append((currentPerson, currentIdx))
 					}
 				}
 			}
 		}
 		
-		return res
+		return res // Возвращаем здоровых соседей
 	}
 }
--- EpidemyIO/ViewModel/SImulationViewModel.swift
+++ EpidemyIO/ViewModel/SImulationViewModel.swift
@@ -11,45 +11,70 @@ class SimulationViewModel: ObservableObject {
 	
 	var simulationModel: SimulationModel?
 	
+	// MARK: Заполняемые пользователем поля
+	// Т.к. ViewModel создается еще до того как будет использована
+	// на экране заполнения данных симуляции, то создаем все поля Optional<Int>
 	@Published var groupSize: Int?
 	@Published var infectionInterval: Int?
 	@Published var infectionFactor: Int?
 	
-	var group: [Person] = []
+	// MARK: Счетчики и обработка состояний для View
+	// Аналогично сказанному выше выставляем дефолтное состояние и счетчики
 	@Published var healthyPeople = 0
 	@Published var infectedPeople = 0
-	
 	@Published var state: State = .idle
+	
+	// MARK: Массив людей для View
+	var group: [Person] = []
 	
 	enum State {
 		case loading
 		case idle
 	}
 	
+	// MARK: Метод запуска симуляции
+	// Далее создаем модель по полученным данным с полей ввода и запускаем ее с помощью .startSimulation()
+	//
+	// корректное количество зараженных и больных людей, так как вся подобная информация хранится исключительно в моделе
+	// С группой аналогичная ситуация. Так как сама группая хранится в моделе и создается в background потоке, то мы
+	// возвращаем ее ViewModel, чтобы ViewModel могла передавать View корректную группу для отображения
 	func startSimulation() {
 		self.state = .loading
 		
+		// Обрабатываем nil поля, чтобы случайно не пердать их в модель
 		guard let groupSize = groupSize, let infectionInterval = infectionInterval, let infectionFactor = infectionFactor else {
 			self.state = .idle
 			return
 		}
 		
+		// Далее создаем модель по полученным данным с полей ввода
 		self.simulationModel = SimulationModel(groupSize: groupSize, infectionInterval: infectionInterval, InfectionFactor: infectionFactor, columns: 10) { infectedPeople, healthyPeople in
+			//Получаем от модели актуальное состояние счетчиков c помощью callback'ов
 			self.infectedPeople = infectedPeople
 			self.healthyPeople = healthyPeople
 		}
 		
+		// Создаем группы и возвращаем callback'ом когда она будет готова
 		self.simulationModel?.createGroup { group in
 			self.group = group
 			self.healthyPeople = group.count
 			self.state = .idle
 		}
+		
+		// Запускаем модель, когда она создана и получила все нужные данные
+		self.simulationModel?.startSimulation()
 	}
 	
+	// MARK: Метод остановки симуляции
 	func stopSimulation() {
 		simulationModel?.stopSimulation()
 	}
 	
+	// MARK: Метод заражения человека
+	// С помощью этого метода мы заражаем конкретного человека на которого мы нажали в нашем View
+	// Он нужен для того, чтобы передать моделе информации о том кого именно нужно заразить в нашей группе
+	// Далее модель сама выполнит операцию по заражению, пересчитает количество больных и здоровых людей и вернет нам эти
+	// значения для наших счетчиков
 	func infect(personIndex: Int) {
 		self.simulationModel?.addToInfectors(infectorIndex: personIndex) { infectedPeople, healthyPeople in
 			self.infectedPeople = infectedPeople
